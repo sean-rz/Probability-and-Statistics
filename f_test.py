9f99cca13504550830a330881adf708063feebb0
Soroosh Rezazadeh <soroosh.rezazadeh@gmail.com>
2018-11-29 14:01:36 -0500

    F-test

--- f_test.py
+++ f_test.py
@@ -4,7 +4,14 @@ alpha = 0.05
 # dfn = df_groups = n_groups - 1
 # dfd = df_error = (n_rows - 1) * n_groups
 
-f_critical = stats.f.ppf(1-alpha, dfn=2, dfd=27)
+a = [11, 16, 9, 14, 10]   # 2% discount
+b = [21, 15, 23, 10, 16]  # 1% discount
+c = [14, 11, 18, 16, 21]  # no discount
+
+f_value = stats.f_oneway(a,b,c).statistic
+print(f_value)    # 2.121212121212121
+f_critical = stats.f.ppf(1-alpha, dfn=2, dfd=12)
+print(f_critical) # 3.8852938346523933
 
 '''
 If f_value < f_critical, we fail to reject the null hypothesis
